8d3baeb4996f2294e1c6abfd84c3b0e9ff2c61e7
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2025-01-26 15:27:35 +0300

    feat: add ability to process letters and numbers

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -29,6 +29,8 @@ func (l *Lexer) readChar() {
 func (l *Lexer) nextToken() token.Token {
 	var tok token.Token
 
+	l.skipWhitespace()
+
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
@@ -49,6 +51,18 @@ func (l *Lexer) nextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 
 	l.readChar()
@@ -57,4 +71,39 @@ func (l *Lexer) nextToken() token.Token {
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func (l *Lexer) readIdentifier() string {
+	startPosition := l.position
+
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[startPosition:l.position]
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		// skip forward once
+		l.readChar()
+	}
+}
+
+func (l *Lexer) readNumber() string {
+	currentPosition := l.position
+
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[currentPosition:l.position]
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
 }
